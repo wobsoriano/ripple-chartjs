c0795d936ece5c20e1543919bcdce066f8adebe3
wobsoriano <sorianorobertc@gmail.com>
2025-09-16 22:45:04 -0700

    omit type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,11 +4,11 @@ import type { ChartProps } from "../lib/types";
 
 export declare function Chart(props: ChartProps<ChartType, DefaultDataPoint<ChartType>>)
 
-export declare function Line(props: ChartProps<'line', DefaultDataPoint<'line'>>): void;
-export declare function Bar(props: ChartProps<'bar', DefaultDataPoint<'bar'>>): void;
-export declare function Radar(props: ChartProps<'radar', DefaultDataPoint<'radar'>>): void;
-export declare function Doughnut(props: ChartProps<'doughnut', DefaultDataPoint<'doughnut'>>): void;
-export declare function PolarArea(props: ChartProps<'polarArea', DefaultDataPoint<'polarArea'>>): void;
-export declare function Bubble(props: ChartProps<'bubble', DefaultDataPoint<'bubble'>>): void;
-export declare function Pie(props: ChartProps<'pie', DefaultDataPoint<'pie'>>): void;
-export declare function Scatter(props: ChartProps<'scatter', DefaultDataPoint<'scatter'>>): void;
+export declare function Line(props: Omit<ChartProps<'line', DefaultDataPoint<'line'>>, '$type'>): void;
+export declare function Bar(props: Omit<ChartProps<'bar', DefaultDataPoint<'bar'>>, '$type'>): void;
+export declare function Radar(props: Omit<ChartProps<'radar', DefaultDataPoint<'radar'>>, '$type'>): void;
+export declare function Doughnut(props: Omit<ChartProps<'doughnut', DefaultDataPoint<'doughnut'>>, '$type'>): void;
+export declare function PolarArea(props: Omit<ChartProps<'polarArea', DefaultDataPoint<'polarArea'>>, '$type'>): void;
+export declare function Bubble(props: Omit<ChartProps<'bubble', DefaultDataPoint<'bubble'>>, '$type'>): void;
+export declare function Pie(props: Omit<ChartProps<'pie', DefaultDataPoint<'pie'>>, '$type'>): void;
+export declare function Scatter(props: Omit<ChartProps<'scatter', DefaultDataPoint<'scatter'>>, '$type'>): void;
