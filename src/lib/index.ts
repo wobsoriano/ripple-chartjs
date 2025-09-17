b9fc510ceef0c3f53630eb09633687d7e2163259
wobsoriano <sorianorobertc@gmail.com>
2025-09-16 22:49:18 -0700

    expect ts errors

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,5 @@
+// @ts-expect-error: known issue, currently being worked on
 export { Chart } from './chart.ripple';
+
+// @ts-expect-error: known issue, currently being worked on
 export { Line, Bar, Radar, Doughnut, PolarArea, Bubble, Pie, Scatter } from './typedCharts.ripple';
